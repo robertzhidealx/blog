da113d55b0ed879b3d863bd1b0e56ce5a678dc44
Robert Zhang <jiaxuanzhang066@gmail.com>
2022-07-09 18:50:54 -0700

    chore: tweak bio

--- theme.config.js
+++ theme.config.js
@@ -1,3 +1,7 @@
 export default {
-  footer: <p style={{paddingTop: '5rem', color: 'gray', fontSize: '0.8rem'}}>MIT {(new Date).getFullYear()} © Jiaxuan Zhang.</p>,
-}
+  footer: (
+    <p style={{ paddingTop: "32px", color: "gray", fontSize: "0.8rem" }}>
+      MIT {new Date().getFullYear()} © Robert Zhang.
+    </p>
+  ),
+};
